538ffbba29ba64f0535bac9a215158f8955600cf
antcont <antoniogiovannicontarino@gmail.com>
2021-06-12 00:47:04 +0200

    optimised lemmatisation

--- lemmatisation_TreeTagger.py
+++ lemmatisation_TreeTagger.py
@@ -3,21 +3,31 @@
 """
 import treetaggerwrapper
 
-input = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\merged_termlist_id_m-n.txt"
-output_lemmatised = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\merged_termlist_id_m-n_lemmatised_TT.txt"
+input = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\mmt_baseline_1.txt"
+output_lemmatised = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\mmt_baseline_1_lemmatised.txt"
+
+tagger_it = treetaggerwrapper.TreeTagger(TAGLANG="it")
+tagger_de = treetaggerwrapper.TreeTagger(TAGLANG="de")
+
 
 def lemmatise_old(sent, lang):
-    lemmatiser = treetaggerwrapper.TreeTagger(TAGLANG=lang)
-    tags = lemmatiser.tag_text(sent)
+    if lang == "it":
+        tagger = tagger_it
+    elif lang == "de":
+        tagger = tagger_de
+    tags = tagger.tag_text(sent)
     tags2 = treetaggerwrapper.make_tags(tags, allow_extra=True)
     lemmatised_sent = " ".join([item.lemma for item in tags2])
     return lemmatised_sent
 
 
 def lemmatise(text, lang):
-    tagger = treetaggerwrapper.TreeTagger(TAGLANG=lang)
+    if lang == "it":
+        tagger = tagger_it
+    elif lang == "de":
+        tagger = tagger_de
     tags = tagger.tag_text(text)
-    mytags = treetaggerwrapper.make_tags(tags, allow_extra=True)
+    mytags = treetaggerwrapper.make_tags(tags, exclude_nottags=True)
     lemma_list = []
     for tag in mytags:
         try:
@@ -28,10 +38,17 @@ def lemmatise(text, lang):
     return " ".join(lemma_list)
 
 
-
-#  importing list of it-de terms
 with open(input, "r", encoding="utf-8") as inputText:
     textLines = inputText.read().splitlines()
+
+textLemma = []
+for it_de in textLines:
+    lemmas_de = lemmatise(it_de, "de")
+    textLemma.append(lemmas_de)
+with open(output_lemmatised, "w", encoding="utf-8") as lemmatised:
+    lemmatised.write("\n".join(textLemma))
+
+    """
     '''textLemma = []
     for it_de in textLines:
         lemmas_it = lemmatise((it_de.split("\t")[0]), "it")
@@ -64,7 +81,7 @@ for id, (it, de) in id_terms.items():
 
 with open(output_lemmatised, "w", encoding="utf-8") as exp:
     exp.write("\n".join(export_as_text))
-
+"""
 
 
 
