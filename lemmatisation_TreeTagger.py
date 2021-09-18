a74ebd1455aca4fadf058c2180d9f8307a38f9b0
antcont <antoniogiovannicontarino@gmail.com>
2021-09-18 20:57:38 +0200

    varia
    updated LexTermEval
    removed unused scripts
    removed HanTa lemmatiser
    
    minor fixes

--- lemmatisation_TreeTagger.py
+++ lemmatisation_TreeTagger.py
@@ -1,10 +1,10 @@
 """
-
+Treetagger lemmatiser
 """
 import treetaggerwrapper
 
-input = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\mmt_baseline_1.txt"
-output_lemmatised = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\mmt_baseline_1_lemmatised.txt"
+input = r""
+output_lemmatised = r""
 
 tagger_it = treetaggerwrapper.TreeTagger(TAGLANG="it")
 tagger_de = treetaggerwrapper.TreeTagger(TAGLANG="de")
@@ -42,46 +42,12 @@ with open(input, "r", encoding="utf-8") as inputText:
     textLines = inputText.read().splitlines()
 
 textLemma = []
-for it_de in textLines:
-    lemmas_de = lemmatise(it_de, "de")
-    textLemma.append(lemmas_de)
+for text in textLines:
+    lemmatised_text = lemmatise(text, "de")
+    textLemma.append(lemmatised_text)
 with open(output_lemmatised, "w", encoding="utf-8") as lemmatised:
     lemmatised.write("\n".join(textLemma))
 
-    """
-    '''textLemma = []
-    for it_de in textLines:
-        lemmas_it = lemmatise((it_de.split("\t")[0]), "it")
-        lemmas_de = lemmatise((it_de.split("\t")[1]), "de")
-        textLemma.append("%s\t%s" % (lemmas_it, lemmas_de))'''
-    it_de = []
-    for biterm in textLines:
-        it_de.append(tuple(biterm.split("\t")))  # creating list of tab-separated ID-ItTerms-DeTerms
-    id_terms = {}
-    for (id, termsIt, termsDe) in it_de:
-        termsIt = [lemmatise(x, "it") for x in termsIt.split("|")]
-        termsDe = [lemmatise(x, "de") for x in termsDe.split("|")]
-
-        id_terms[id] = (termsIt, termsDe)
-
-
-
-'''with open(output_lemmatised, "w", encoding="utf-8") as lemmatised:
-    lemmatised.write("\n".join(textLemma))'''
-
-export_as_text = []
-for id, (it, de) in id_terms.items():
-    it_terms = []
-    de_terms = []
-    for itterm in it:
-        it_terms.append(itterm)
-    for determ in de:
-        de_terms.append(determ)
-    export_as_text.append("%s\t%s\t%s" % (id, "|".join(it_terms), "|".join(de_terms)))
-
-with open(output_lemmatised, "w", encoding="utf-8") as exp:
-    exp.write("\n".join(export_as_text))
-"""
 
 
 
--- tok_detok.py
+++ tok_detok.py
@@ -3,21 +3,32 @@ script for sentence tokenization and detokenization (using the TreeTagger)
 this is needed in order to have all sentences of the test set (non lemmatised vs lemmatised) with the same tokenization
 in order to allow retrieving matched terms between non-lemmatised and lemmatised sentences using match span position
 
-the script also creates the final test set file
-id - src - ref - src_lemma - ref_lemma
+The script tokenizes and detokenizes the non-lemmatised sentences and creates the final test set file
+id - src - ref - hyp - src_lemma - ref_lemma - hyp_lemma
 
 
 
 '''
 import treetaggerwrapper
 
-input_totokenize = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\testset+reference_2000_1 - Copia_todetokenize.txt"
-input_lemmatised = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\testset+reference_2000_1_lemmatised_TreeTagger.txt"
-output_detokenized = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\testset+reference_2000_1 - detokenized.txt"
+src_totokenize = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_source_2000.txt"
+ref_totokenize = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_reference_2000.txt"
+hyp_totokenize = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_translated_2000_custom4a+termini.txt"
+src_lemma = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_source_2000_lemmatised.txt"
+ref_lemma = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_reference_2000_lemmatised.txt"
+hyp_lemma = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_translated_2000_custom4a+termini_lemmatised.txt"
+output_testset = r"C:\Users\anton\Documents\Documenti importanti\SSLMIT FORLI M.A. SPECIALIZED TRANSLATION 2019-2021\tesi\Evaluation (Automatic + Manual)\Esperimento definitivo tesi\NEW_testSet_custom4a+terminology_LexTermEval.txt"
+
+#  instantiating taggers for each language
+tagger_it = treetaggerwrapper.TreeTagger(TAGLANG="it")
+tagger_de = treetaggerwrapper.TreeTagger(TAGLANG="de")
 
 
 def tokenize_detokenize(text, lang):
-    tagger = treetaggerwrapper.TreeTagger(TAGLANG=lang)
+    if lang == "it":
+        tagger = tagger_it
+    elif lang == "de":
+        tagger = tagger_de
     tags = tagger.tag_text(text)
     mytags = treetaggerwrapper.make_tags(tags)
     tokenized_list = []
@@ -30,10 +41,54 @@ def tokenize_detokenize(text, lang):
     return " ".join(tokenized_list)
 
 
-with open(input_totokenize, "r", encoding="utf-8") as inputText:
-    textLines = inputText.read().splitlines()
+#  data to tokenize/detokenize
+with open(src_totokenize, "r", encoding="utf-8") as src_tok:
+    src_t = src_tok.read().splitlines()
+with open(ref_totokenize, "r", encoding="utf-8") as ref_tok:
+    ref_t = ref_tok.read().splitlines()
+with open(hyp_totokenize, "r", encoding="utf-8") as hyp_tok:
+    hyp_t = hyp_tok.read().splitlines()
 
-with open(input_lemmatised, "r", encoding="utf-8") as x:
+#  lemmmatised data to append to test set
+with open(src_lemma, "r", encoding="utf-8") as src_lemma:
+    src_l = src_lemma.read().splitlines()
+with open(ref_lemma, "r", encoding="utf-8") as ref_lemma:
+    ref_l = ref_lemma.read().splitlines()
+with open(hyp_lemma, "r", encoding="utf-8") as hyp_lemma:
+    hyp_l = hyp_lemma.read().splitlines()
+
+
+#  tokenizing / detokenizing
+detok_src = []
+for line in src_t:
+    detok_src.append(tokenize_detokenize(line, "it"))
+
+detok_ref = []
+for line in ref_t:
+    detok_ref.append(tokenize_detokenize(line, "de"))
+
+detok_hyp = []
+for line in hyp_t:
+    detok_hyp.append(tokenize_detokenize(line, "de"))
+
+#  check lengths
+if not len(src_l) == len(ref_l) == len(hyp_l) == len(detok_src) == len(detok_ref) == len(detok_hyp):
+    print("An error occurred. Different sentence length between source and reference.")
+    exit()
+
+
+#  writing test set output file
+testset_out = []
+for i in range(len(src_t)):
+    row = "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (i+1, detok_src[i], detok_ref[i], detok_hyp[i], src_l[i], ref_l[i],
+                                          hyp_l[i])
+    testset_out.append(row)
+    print(row)
+
+
+
+
+'''with open(input_lemmatised, "r", encoding="utf-8") as x:
     textLemma =x.read().splitlines()
 
 src = []
@@ -51,17 +106,17 @@ for line in textLemma:
     src_lemma.append(srcL)
     ref_lemma.append(refL)
 
-#  check lengths
-if not len(src) == len(ref) == len(src_lemma) == len(ref_lemma):
-    print("An error occurred. Different sentence length between source and reference.")
-    #exit()
+
 
 output_list = []
 for i in range(len(src)):
     output_list.append("%s\t%s\t%s\t%s\t%s" % (i, src[i], ref[i], src_lemma[i], ref_lemma[i]))
     print("%s\t%s\t%s\t%s\t%s" % (i, src[i], ref[i], src_lemma[i], ref_lemma[i]))
 
-with open(output_detokenized, "w", encoding="utf-8") as out:
-    out.write("\n".join(output_list))
+'''
+
+
+with open(output_testset, "w", encoding="utf-8") as out:
+    out.write("\n".join(testset_out))
 
 print("Done.")
